0395ec3f5e0871fdff3cc4ab1bad2f14be6d6148
Mattdwest <71993590+Mattdwest@users.noreply.github.com>
2020-11-22 16:43:48 -0800

    uploading curve files

---
+++ contracts/strategies/Strategy3PoolMTA.sol
@@ -0,0 +1,164 @@
+// SPDX-License-Identifier: MIT
+
+pragma experimental ABIEncoderV2;
+pragma solidity 0.6.12;
+
+import "@openzeppelinV3/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelinV3/contracts/math/SafeMath.sol";
+import "@openzeppelinV3/contracts/math/Math.sol";
+import "@openzeppelinV3/contracts/utils/Address.sol";
+import "@openzeppelinV3/contracts/token/ERC20/SafeERC20.sol";
+import {BaseStrategy, StrategyParams} from "@yearnvaults/contracts/BaseStrategy.sol";
+
+import "../../interfaces/curve/ICurve.sol";
+import "../../interfaces/curve/IGauge.sol";
+import "../../interfaces/uniswap/Uni.sol";
+import "../../interfaces/yearn/Vault.sol";
+
+
+contract Strategy3PoolMTA is BaseStrategy {
+    using SafeERC20 for IERC20;
+    using Address for address;
+    using SafeMath for uint256;
+
+    address public mta;
+    address public threePool;
+    address public crv;
+    address public unirouter;
+    address public threePoolMTA;
+    address public gauge;
+    string public constant override name = "Strategy3PoolMTA";
+
+    constructor(
+        address _vault,
+        address _mta,
+        address _threePool,
+        address _crv,
+        address _threePoolMTA,
+        address _gauge,
+        address _unirouter
+    ) public BaseStrategy(_vault) {
+        mta = _mta;
+        threePool = _threePool;
+        crv = _crv;
+        threePoolMTA = _threePoolMTA;
+        gauge = _gauge;
+        unirouter = _unirouter;
+
+        IERC20(threePool).safeApprove(threePoolMTA, uint256(-1));
+        IERC20(threePoolMTA).safeApprove(gauge, uint256(-1));
+        IERC20(mta).safeApprove(unirouter, uint256(-1));
+        IERC20(crv).safeApprove(unirouter, uint256(-1));
+    }
+
+    function protectedTokens() internal override view returns (address[] memory) {
+        address[] memory protected = new address[](5);
+        protected[0] = threePool;
+        protected[1] = threePoolMTA;
+        protected[2] = mta;
+        protected[3] = crv;
+        protected[4] = address(want); // want is usdt
+        return protected;
+    }
+
+    // returns sum of all assets, realized and unrealized
+    function estimatedTotalAssets() public override view returns (uint256) {
+        return balanceOfWant().add(balanceOfStake()).add(balanceOfPool());
+    }
+
+    function prepareReturn(uint256 _debtOutstanding) internal override returns (uint256 _profit) {
+       // We might need to return want to the vault
+        if (_debtOutstanding > 0) {
+           liquidatePosition(_debtOutstanding);
+        }
+
+        // Update reserve with the available want so it's not considered profit
+        setReserve(balanceOfWant().sub(_debtOutstanding));
+
+        // claim/sell MTA and curve
+
+
+        // Final profit is want generated in the swap if ethProfit > 0
+        _profit = balanceOfWant().sub(getReserve());
+    }
+
+    //todo: this
+    function adjustPosition(uint256 _debtOutstanding) internal override {
+       //emergency exit is dealt with in prepareReturn
+        if (emergencyExit) {
+          return;
+       }
+
+        // Reset the reserve value before
+        setReserve(0);
+
+       // Invest the rest of the want
+       uint256 _wantAvailable = balanceOfWant().sub(_debtOutstanding);
+        if (_wantAvailable > 0) {
+            uint256 _availableFunds = IERC20(usdt).balanceOf(address(this));
+            ICurve(threePool).add_liquidity([0,0,_availableFunds], 0);
+            Vault(y3Pool).depositAll();
+        }
+    }
+
+    // withdraws everything that is currently in the strategy, regardless of values.
+    function exitPosition() internal override {
+        //uint256 y3PoolBalance = IERC20(y3Pool).balanceOf(address(this));
+        uint256 gaugeBalance = IGauge(gauge).balanceOf(address(this));
+        IGauge(gauge).withdraw(gaugeBalance);
+        uint256 threePoolMTABalance = IERC20(threePoolMTA).balanceOf(address(this));
+        ICurve(threePoolMTA).remove_liquidity_one_coin(threePoolMTABalance, 2, 0);
+        }
+
+    //this math only deals with want, which is 3pool.
+    function liquidatePosition(uint256 _amountNeeded) internal override returns (uint256 _amountFreed) {
+        if (balanceOfWant() < _amountNeeded) {
+            // We need to sell stakes to get back more want
+            _withdrawSome(_amountNeeded.sub(balanceOfWant()));
+        }
+
+        // Since we might free more than needed, let's send back the min
+        _amountFreed = Math.min(balanceOfWant(), _amountNeeded);
+    }
+
+
+    // withdraw some usdt from the vaults
+    function _withdrawSome(uint256 _amount) internal returns (uint256) {
+       uint256 _3poolMTAAmount = (_amount).mul(1e18).mul(ICurve(threePoolMTA).get_virtual_price());
+       IGauge(gauge).withdraw(_3PoolMTAAmount);
+       uint256 _3PoolMTABalance = IERC20(threePoolMTA).balanceOf(address(this));
+       uint256 _3PoolMTAPrice = (_3PoolMTABalance).mul(ICurve(threePoolMTA).get_virtual_price());
+       ICurve(threePoolMTA).remove_liquidity_one_coin(_3PoolMTAPrice, 2, 0);
+       uint256 _3PoolBalance = IERC20(threePool).balanceOf(address(this));
+    }
+
+
+    // it looks like this function transfers not just "want" tokens, but all tokens
+    //transfer want, threepoolMTA, crv, mta.
+    function prepareMigration(address _newStrategy) internal override {
+        want.transfer(_newStrategy, balanceOfWant());
+        IERC20(threePoolMTA).transfer(_newStrategy, IERC20(threePoolMTA).balanceOf(address(this)));
+        IERC20(crv).transfer(_newStrategy, IERC20(crv).balanceOf(address(this)));
+        IERC20(mta).transfer(_newStrategy, IERC20(mta).balanceOf(address(this)));
+    }
+
+    // returns value of total 3pool staked in Gauge
+    function balanceOfStake() internal view returns (uint256) {
+        uint256 _balanceG = IGauge(gauge).balanceOf(address(this));
+        return _balanceG;
+    }
+
+    // returns value of total 3pool
+    function balanceOfPool() internal view returns (uint256) {
+        uint256 _balance = IERC20(threePoolMTA).balanceOf(address(this));
+        uint256 _virtualBalance = (_balance).mul(ICurve(threePoolMTA).get_virtual_price());
+        return _virtualBalance;
+    }
+
+    // returns balance of threePool
+    function balanceOfWant() public view returns (uint256) {
+        return IERC20(want).balanceOf(address(this));
+    }
+
+}
+
---
+++ contracts/strategies/StrategyUSDT3Pool.sol
@@ -0,0 +1,146 @@
+// SPDX-License-Identifier: MIT
+
+pragma experimental ABIEncoderV2;
+pragma solidity 0.6.12;
+
+import "@openzeppelinV3/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelinV3/contracts/math/SafeMath.sol";
+import "@openzeppelinV3/contracts/math/Math.sol";
+import "@openzeppelinV3/contracts/utils/Address.sol";
+import "@openzeppelinV3/contracts/token/ERC20/SafeERC20.sol";
+import {BaseStrategy, StrategyParams} from "@yearnvaults/contracts/BaseStrategy.sol";
+
+import "../../interfaces/curve/ICurve.sol";
+import "../../interfaces/yearn/Vault.sol";
+
+
+contract StrategyUSDT3Pool is BaseStrategy {
+    using SafeERC20 for IERC20;
+    using Address for address;
+    using SafeMath for uint256;
+
+    address public usdt;
+    address public threePool;
+    address public y3Pool;
+    address public unirouter;
+    string public constant override name = "StrategyUSDT3Pool";
+
+    constructor(
+        address _vault,
+        address _usdt,
+        address _threePool,
+        address _y3Pool
+    ) public BaseStrategy(_vault) {
+        usdt = _usdt;
+        threePool = _threePool;
+        y3Pool = _y3Pool;
+
+        IERC20(usdt).safeApprove(threePool, uint256(-1));
+        IERC20(threePool).safeApprove(y3Pool, uint256(-1));
+    }
+
+    function protectedTokens() internal override view returns (address[] memory) {
+        address[] memory protected = new address[](4);
+        protected[0] = usdt;
+        protected[1] = threePool;
+        protected[2] = y3Pool;
+        protected[3] = address(want); // want is usdt
+        return protected;
+    }
+
+    // returns sum of all assets, realized and unrealized
+    function estimatedTotalAssets() public override view returns (uint256) {
+        return balanceOfWant().add(balanceOfStake()).add(balanceOfPool());
+    }
+
+    function prepareReturn(uint256 _debtOutstanding) internal override returns (uint256 _profit) {
+       // We might need to return want to the vault
+        if (_debtOutstanding > 0) {
+           liquidatePosition(_debtOutstanding);
+        }
+
+        // Update reserve with the available want so it's not considered profit
+        setReserve(balanceOfWant().sub(_debtOutstanding));
+
+        // Final profit is want generated in the swap if ethProfit > 0
+        _profit = balanceOfWant().sub(getReserve());
+    }
+
+    function adjustPosition(uint256 _debtOutstanding) internal override {
+       //emergency exit is dealt with in prepareReturn
+        if (emergencyExit) {
+          return;
+       }
+
+        // Reset the reserve value before
+        setReserve(0);
+
+       // Invest the rest of the want
+       uint256 _wantAvailable = balanceOfWant().sub(_debtOutstanding);
+        if (_wantAvailable > 0) {
+            uint256 _availableFunds = IERC20(usdt).balanceOf(address(this));
+            ICurve(threePool).add_liquidity([0,0,_availableFunds], 0);
+            Vault(y3Pool).depositAll();
+        }
+    }
+
+    // withdraws everything that is currently in the strategy, regardless of values.
+    // todo: check that usdt is #3 in pool
+    function exitPosition() internal override {
+        //uint256 y3PoolBalance = IERC20(y3Pool).balanceOf(address(this));
+        Vault(y3Pool).withdrawAll();
+        uint256 threePoolBalance = IERC20(threePool).balanceOf(address(this));
+        ICurve(threePool).remove_liquidity_one_coin(threePoolBalance, 3, 0);
+        }
+
+    //this math only deals with want, which is usdt.
+    function liquidatePosition(uint256 _amountNeeded) internal override returns (uint256 _amountFreed) {
+        if (balanceOfWant() < _amountNeeded) {
+            // We need to sell stakes to get back more want
+            _withdrawSome(_amountNeeded.sub(balanceOfWant()));
+        }
+
+        // Since we might free more than needed, let's send back the min
+        _amountFreed = Math.min(balanceOfWant(), _amountNeeded);
+    }
+
+
+    // withdraw some usdt from the vaults
+    // TODO: verify that USDT is #3 in the onecoin function
+    function _withdrawSome(uint256 _amount) internal returns (string memory) {
+        uint256 _3PoolAmount = (_amount).mul(1e18).div(ICurve(threePool).get_virtual_price());
+        uint256 y3PoolAmount = (_3PoolAmount).mul(1e18).div(Vault(y3Pool).getPricePerFullShare());
+        Vault(y3Pool).withdraw(y3PoolAmount);
+        uint256 threePoolBalance = IERC20(threePool).balanceOf(address(this));
+        ICurve(threePool).remove_liquidity_one_coin(threePoolBalance, 3, 0);
+    }
+
+
+    // it looks like this function transfers not just "want" tokens, but all tokens
+    function prepareMigration(address _newStrategy) internal override {
+        want.transfer(_newStrategy, balanceOfWant());
+        IERC20(threePool).transfer(_newStrategy, IERC20(threePool).balanceOf(address(this)));
+        IERC20(y3Pool).transfer(_newStrategy, IERC20(y3Pool).balanceOf(address(this)));
+    }
+
+    // returns value of total 3pool
+    function balanceOfPool() internal view returns (uint256) {
+        uint256 _balance = IERC20(threePool).balanceOf(address(this));
+        uint256 ratio = ICurve(threePool).get_virtual_price();
+        return (_balance).mul(ratio);
+    }
+
+    // returns value of total 3pool in vault
+    function balanceOfStake() internal view returns (uint256) {
+        uint256 _balance = IERC20(y3Pool).balanceOf(address(this));
+        uint256 ratio = Vault(y3Pool).getPricePerFullShare();
+        return (_balance).mul(ratio);
+    }
+
+    // returns balance of usdt
+    function balanceOfWant() public view returns (uint256) {
+        return IERC20(want).balanceOf(address(this));
+    }
+
+}
+
